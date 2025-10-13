5e7681834042f7533a066c83c839a7512c2a5a2e
v01d.dev <v01d.dev.sh@gmail.com>
2025-10-13 22:20:12 +0300

    feat: findian

---
+++ findian/findian.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var input string
+	fmt.Scan(&input)
+	input = strings.ToLower(input)
+	
+	if strings.HasPrefix(input, "i") && strings.Contains(input, "a") && strings.HasSuffix(input, "n") {
+		fmt.Println("Found!")
+	} else {
+		fmt.Println("Not Found!")
+	}
+}
